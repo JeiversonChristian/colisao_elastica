163dc585d12d33229e9c2f7ca8176802085caad3
JeiversonChristian <jeiversonc@gmail.com>
2024-07-29 02:17:03 -0300

    concluindo o projeto

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
 # Colisão Elástica
 ### Programa que simula a colisão elástica de duas massas
 
-## Colocar a explicação teórica depois:
-
-blá blá blá
-
-## Colocar imagem da simulação depois
-
-![software_print]()
-![software_print]()
-![software_print]()
+![software_print](explicacao.png)
 
 ## Teste você mesmo no link:
 
-[Link da simulação](https://jeiversonchristian.github.io/colisao_celulas/)
+[Link da simulação](https://jeiversonchristian.github.io/colisao_elastica/)
--- main.js
+++ main.js
@@ -14,6 +14,7 @@ window.onload = function() {
     let deu_play = false;
     let deu_pause = false;
     let deu_reestart = false;
+    let acabou = false
     // --------------------------------------------------------------------------------
     
     // Densidades dos materiais -------------------------------------------------------
@@ -55,7 +56,7 @@ window.onload = function() {
     dist_parede1 = c1r // distancia até parede
     let c1x = 0 + c1r + dist_parede1 // coordenada x do centro da esfera
     let c1y = window.canvas.height / 2 // coordenada y do centro da esfera
-    let c1v = 0; // velocidade da esfera que se atualiza
+    let c1v = 10; // velocidade da esfera que se atualiza
     let c1vi = c1v; // velocidade inicial que não se altera
     let c1c = 'blue' // cor da esfera
     let pode_atualizar_pos1 = true; // auxilar para permitir movimentação
@@ -67,13 +68,24 @@ window.onload = function() {
     dist_parede2 = c2r
     let c2x = window.canvas.width - c2r - dist_parede2
     let c2y = window.canvas.height / 2
-    let c2v = -0
+    let c2v = -10
     let c2vi = c2v;
     let c2c = 'red'
     let pode_atualizar_pos2 = true;
     let c2_colidiu = false;
     // --------------------------------------------------------------------------------
 
+    // Sons ---------------------------------------------------------------------------
+    const som_botao = new Audio('botao.mp3');
+    som_botao.volume = 0.1;
+    const som_erro = new Audio('erro.mp3');
+    som_erro.volume = 0.1;
+    const som_colisao = new Audio('colisao.mp3');
+    som_colisao.volume = 0.1;
+    const som_aee = new Audio('aee.mp3');
+    som_aee.volume = 0.1;
+    // --------------------------------------------------------------------------------
+    
     // --------------------------------------------------------------------------------
     function desenhar_esferas(){
         // esfera 1 
@@ -133,7 +145,7 @@ window.onload = function() {
 
     // --------------------------------------------------------------------------------
     function verificar_colisao(){
-        if (deu_pause == false){
+        if (deu_play == true && deu_pause == false){
             // se as duas esferascolidem
             if ((c1x + c1r) >= (c2x - c2r)) {
                 // calcula as velocidades novas
@@ -153,6 +165,7 @@ window.onload = function() {
                 y = (2*a*c - a*d + b*d)/(a + b)
                 c1v = x
                 c2v = y
+                som_colisao.play();
                 c1_colidiu = true;
                 c2_colidiu = true;
 
@@ -189,24 +202,31 @@ window.onload = function() {
         ctx.fillStyle = "white";
         ctx.textAlign = "center";
         ctx.fillText(`Colisão Elástica`,window.canvas.width/2,50);
+
+        //Texto explicativo
+        ctx.font = "20px Arial";
+        ctx.fillText(`Altere os valores abaixo ANTES de dar play e veja o que acontece.`,window.canvas.width/2,90);
+
         // Esferas
         ctx.font = "20px Arial";
         // Esfera 1
         ctx.textAlign = "left";
+        ctx.fillText(`posição inicial`,60,window.canvas.height-82);
         ctx.fillText(`m1 = ${m1} kg`,60,window.canvas.height-57);
         ctx.fillText(`v1i = ${c1vi} m/s`,60,window.canvas.height-32);
         if (c1_colidiu == false) {
-            ctx.fillText(`v1f: "A Definir"`,50,window.canvas.height-7);
+            ctx.fillText(`v1f: "Veja após a Colisão"`,60,window.canvas.height-7);
         }
         else{
-            ctx.fillText(`v1f ≅ ${c1v.toFixed(2)} m/s`,50,window.canvas.height-7);
+            ctx.fillText(`v1f ≅ ${c1v.toFixed(2)} m/s`,60,window.canvas.height-7);
         }
         // Esfera 2
         ctx.textAlign = "right";
+        ctx.fillText(`posição inicial`,window.canvas.width-60,window.canvas.height-82);
         ctx.fillText(`m2 = ${m2} kg`,window.canvas.width-60,window.canvas.height-57);
         ctx.fillText(`v2i = ${c2vi} m/s`,window.canvas.width-60,window.canvas.height-32);
         if (c1_colidiu == false) {
-            ctx.fillText(`v1f: "A Definir"`,window.canvas.width-60,window.canvas.height-7);
+            ctx.fillText(`v2f: "Veja após a Colisão"`,window.canvas.width-60,window.canvas.height-7);
         }
         else{
             ctx.fillText(`v2f ≅ ${c2v.toFixed(2)} m/s`,window.canvas.width-60,window.canvas.height-7);
@@ -218,30 +238,47 @@ window.onload = function() {
     function verificar_botoes(){
         //play
         document.getElementById('play').addEventListener('click', () => {
-            deu_play = true;
-            deu_pause = false;
-            deu_reestart = false;
+            if (acabou == true){
+                som_erro.play();
+                window.alert("Reinicie!");
+            }
+            else{
+                som_botao.play();
+                deu_play = true;
+                deu_pause = false;
+                deu_reestart = false;
+            }
         });
 
         //pause
         document.getElementById('pause').addEventListener('click', () => {
-            deu_pause = true;
+            if (acabou == true){
+                som_erro.play();
+                window.alert("Reinicie!");
+            }
+            else{
+                som_botao.play();
+                deu_pause = true;
+            }
         });
 
         //reestart
         document.getElementById('reestart').addEventListener('click', () => {
+            som_botao.play();
             m1 = 10;
             c1r = (((((3*m1)/(4*Math.PI*p))**(1/3))*100)/10)*37.8;
+            dist_parede1 = c1r;
             c1x = 0 + c1r + dist_parede1;
-            c1v = 0;
+            c1v = 10;
             c1vi = c1v;
             pode_atualizar_pos1 = true;
             c1_colidiu = false;
 
             m2 = 10;
             c2r = (((((3*m2)/(4*Math.PI*p))**(1/3))*100)/10)*37.8;
+            dist_parede2 = c2r;
             c2x = window.canvas.width - c2r - dist_parede2;
-            c2v = 0;
+            c2v = -10;
             c2vi = c2v;
             pode_atualizar_pos2 = true;
             c2_colidiu = false;
@@ -249,34 +286,56 @@ window.onload = function() {
             deu_reestart = true;
             deu_play = false;
             deu_pause = false;
+
+            acabou = false;
         });
 
         // so pode mexer nos parâmetros das esferas com o jogo pausado
-        // se apertar o botão, aumenta ou diminui o valor (massa/velocidade)
+        // se apertar o botão, aumenta ou diminui (massa/velocidade/dist_parede)
         // massa
         document.getElementById('increment3').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (m1 >= 10 && m1 <= 980 && deu_play == false) {
+                som_botao.play();
                 m1 += 10;
                 c1r = (((((3*m1)/(4*Math.PI*p))**(1/3))*100)/10)*37.8
                 c1x = 0 + c1r + dist_parede1
             }
         });
         document.getElementById('decrement3').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (m1 >= 20 && m1 <= 990 && deu_play == false) {
+                som_botao.play();
                 m1 -= 10;
                 c1r = (((((3*m1)/(4*Math.PI*p))**(1/3))*100)/10)*37.8
                 c1x = 0 + c1r + dist_parede1
             }
         });
         document.getElementById('increment4').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (m2 >= 10 && m2 <= 980 && deu_play == false) {
+                som_botao.play();
                 m2 += 10;
                 c2r = (((((3*m2)/(4*Math.PI*p))**(1/3))*100)/10)*37.8
                 c2x = window.canvas.width - c2r - dist_parede2
             }
         });
         document.getElementById('decrement4').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (m2 >= 20 && m2 <= 990 && deu_play == false) {
+                som_botao.play();
                 m2 -= 10;
                 c2r = (((((3*m2)/(4*Math.PI*p))**(1/3))*100)/10)*37.8
                 c2x = window.canvas.width - c2r - dist_parede2
@@ -284,36 +343,110 @@ window.onload = function() {
         });
         // velocidade
         document.getElementById('increment1').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (c1v >= 0 && c1v <= 990 && deu_play == false) {
+                som_botao.play();
                 c1v += 10;
                 c1vi = c1v;
             }
         });
         document.getElementById('decrement1').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (c1v >= 10 && c1v <= 1000 && deu_play == false) {
+                som_botao.play();
                 c1v -= 10;
                 c1vi = c1v;
             }
         });
         document.getElementById('increment2').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (c2v <= -10 && c2v >= -990 && deu_play == false) {
+                som_botao.play();
                 c2v += 10;
                 c2vi = c2v;
             }
         });
         document.getElementById('decrement2').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
             if (c2v >= -980 && c2v <= 0 && deu_play == false) {
+                som_botao.play();
                 c2v -= 10;
                 c2vi = c2v;
             }
         });
+        // distância até parede
+        document.getElementById('increment5').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
+            if (c1x - c1r >= 0 && c1x + c1r <= canvas.width/2-c1r-3 && deu_play == false) {
+                som_botao.play();
+                dist_parede1 += c1r;
+                c1x = 0 + c1r + dist_parede1;
+            }
+        });
+        document.getElementById('decrement5').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
+            if (c1x - c1r >= c1r && c1x + c1r <= canvas.width/2 && deu_play == false) {
+                som_botao.play();
+                dist_parede1 -= c1r;
+                c1x = 0 + c1r + dist_parede1;
+            }
+        });
+        document.getElementById('increment6').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
+            if (c2x + c2r <= canvas.width-c2r && dist_parede2 <= canvas.width/2 && deu_play == false) {
+                som_botao.play();
+                dist_parede2 -= c2r;
+                c2x = window.canvas.width - c2r - dist_parede2
+            }
+        });
+        document.getElementById('decrement6').addEventListener('click', () => {
+            if (deu_play == true) {
+                som_erro.play();
+                window.alert('Reinicie para poder alterar');
+            }
+            if (c2x - c2r >= canvas.width/2 + c2r + 3 && dist_parede2 <= canvas.width - c2r -1 && deu_play == false) {
+                som_botao.play();
+                dist_parede2 += c2r;
+                c2x = window.canvas.width - c2r - dist_parede2
+            }
+        });
     }
     // --------------------------------------------------------------------------------
-
+    
+    // --------------------------------------------------------------------------------
+    function verificar_acabou(){
+        if (acabou == false && pode_atualizar_pos1 == false && pode_atualizar_pos2 == false) {
+            acabou = true;
+            som_aee.play();
+        }
+    }
+    // --------------------------------------------------------------------------------
+    
     // --------------------------------------------------------------------------------
     // obs: timestamp já é passado automaticamente pela função requestAnimationFrame
     // Ele é o tempo atual em milissegundos desde o início do tempo em que a página foi carregada
-    function rodar_jogo(timestamp){
+    function rodar_simulacao(timestamp){
         // se já passou mais tempo do que o tamanho de um dos 30 pedacinhos de tempo definidos acima
         if (timestamp - window.lastTime >= window.interval) {
             window.lastTime = timestamp;
@@ -323,12 +456,13 @@ window.onload = function() {
             verificar_colisao();
             desenhar_esferas();
             mostrar_informacoes();
+            verificar_acabou();
         }
-        requestAnimationFrame(rodar_jogo);
+        requestAnimationFrame(rodar_simulacao);
     }
     // --------------------------------------------------------------------------------
 
     desenhar_esferas();
     verificar_botoes();
-    requestAnimationFrame(rodar_jogo);
+    requestAnimationFrame(rodar_simulacao);
 }
